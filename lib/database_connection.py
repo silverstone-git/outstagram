cc6f73b4b050e8a5f7745948168091c061de8b4a
cyto <aryan@csimplifyit.com>
2026-03-26 03:41:24 +0530

    cyto/small db connection refinement

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -5,11 +5,15 @@ from os import getenv
 
 DB_USERNAME = getenv("OUTSTAGRAM_USERNAME", "")
 DB_PASSWORD = getenv("OUTSTAGRAM_PASSWORD", "")
-DB_NAME = getenv("OUTSTAGRAM_DBNAME", "")
+DB_NAME = getenv("OUTSTAGRAM_DBNAME", "outsie")
 DB_HOST = getenv("OUTSTAGRAM_DBHOST", "localhost")
 
-DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/outsie?sslmode=require"
+DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?sslmode=require"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
